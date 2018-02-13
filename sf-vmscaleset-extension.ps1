620eedb4bf72fc956f4d0b77c84fb3ae8ecac4e1
Jean-Francois Guertin <jean-francois.guertin@edwire.com>
2018-02-12 21:24:14 -0800

    Adding SF VM Scale Set Extension Script

---
+++ sf-vmscaleset-extension.ps1
@@ -0,0 +1,9 @@
+﻿#
+# sf-vmscaleset-extension.ps1
+#
+
+# install chocolatey
+(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
+
+# install java
+choco install -y -force javaruntime
