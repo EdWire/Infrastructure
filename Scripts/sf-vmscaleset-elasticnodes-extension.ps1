db2c31d4f1fe8403a821c3e33d8d535933725c4d
Jean-Francois Guertin <jean-francois.guertin@edwire.com>
2018-02-13 18:45:34 -0800

    Adding File Share to VM Extension Script

---
+++ Scripts/sf-vmscaleset-elasticnodes-extension.ps1
@@ -0,0 +1,32 @@
+﻿#
+# sf-vmscaleset-elasticnodes-extension.ps1
+#
+
+#---------------------------
+# Input Parameters
+#---------------------------
+
+Param (
+    [Switch] $sfEsStorageAccountName = $false,
+    [Switch] $sfEsStorageAccountKey = $false,
+    [Switch] $sfEsStorageAccountShareName = $true,
+    [Switch] $driveLetter = "e"
+)
+
+#---------------------------
+# Mount File Share 
+#---------------------------
+
+$acctKey = ConvertTo-SecureString -String "$sfEsStorageAccountKey" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$sfEsStorageAccountName", $acctKey
+New-PSDrive -Name $driveLetter -PSProvider FileSystem -Root "\\$sfEsStorageAccountName.file.core.windows.net\$sfEsStorageAccountShareName" -Credential $credential
+
+#---------------------------
+# Install Java SDK
+#---------------------------
+
+# install chocolatey
+(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
+
+# install java
+choco install -y -force javaruntime
--- Scripts/sf-vmscaleset-extension.ps1
+++
@@ -1,9 +0,0 @@
-﻿#
-# sf-vmscaleset-extension.ps1
-#
-
-# install chocolatey
-(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
-
-# install java
-choco install -y -force javaruntime
