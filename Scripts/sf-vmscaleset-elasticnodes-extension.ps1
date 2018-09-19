26f5eea38ff78f7bcfa1298a9a7eccf37616c453
frenchyjef <jean-francois.guertin@edwire.com>
2018-09-19 12:51:26 -0700

    Update sf-vmscaleset-elasticnodes-extension.ps1
    Adding comments for logging

--- Scripts/sf-vmscaleset-elasticnodes-extension.ps1
+++ Scripts/sf-vmscaleset-elasticnodes-extension.ps1
@@ -1,4 +1,4 @@
-﻿#
+#
 # sf-vmscaleset-elasticnodes-extension.ps1
 #
 
@@ -13,19 +13,42 @@ Param (
 # Mount Data Drive (lun = 0)
 #---------------------------
 
+Write-Output "-------------------------"
+Write-Output "Mounting Data Drive"
+Write-Output "-------------------------"
+
+Write-Output "Get Raw Disk"
+
 $dataDisk0 = Get-Disk | Where-Object {$_.PartitionStyle -eq 'raw' -and ($_.Number -eq 2) }
+
+Write-Output $dataDisk0
+
+Write-Output "Initialize Raw Disk"
 
 $dataDisk0 | 
 Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -UseMaximumSize -DriveLetter "G" |
 Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk0" -Confirm:$false -Force
 
+Write-Output "Completed Mounting Data Drive"
+
+
 #---------------------------
 # Install Java SDK
 #---------------------------
 
+Write-Output "-------------------------"
+Write-Output "Installing Java SDK"
+Write-Output "-------------------------"
+
 # install chocolatey
+Write-Output "Installing Chocolatey"
+
 (iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
 
 # install java
+Write-Output "Installing Java Runtime"
+
 choco install -y -force javaruntime
+
+Write-Output "Completed Installing Java SDK"
