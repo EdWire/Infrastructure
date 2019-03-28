e89c3f982c52cb08568f1ebbfa8ac8a06d40512b
frenchyjef <jean-francois.guertin@edwire.com>
2019-03-28 12:13:29 -0700

    Update sf-vmscaleset-elasticnodes-extension.ps1

--- Scripts/sf-vmscaleset-elasticnodes-extension.ps1
+++ Scripts/sf-vmscaleset-elasticnodes-extension.ps1
@@ -44,7 +44,7 @@ Write-Output "-------------------------"
 # install chocolatey
 Write-Output "Installing Chocolatey"
 
-(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install java
 Write-Output "Installing Java Runtime"
