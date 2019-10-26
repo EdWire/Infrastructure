23d76038282db780a0bbc21bb2e2b47dc1c729dc
Jean-Francois Guertin <jean-francois.guertin@edwire.com>
2019-10-26 08:59:48 -0700

    Test Ed-Fi Policy Deployment Issue

--- Scripts/apim-importapi.ps1
+++ Scripts/apim-importapi.ps1
@@ -8,18 +8,18 @@ Connect-AzAccount
 $context = New-AzApiManagementContext -ResourceGroupName "eg-edgraph-dev-eastus" -ServiceName "eg-apim-dev-eastus"
 
 #Import swagger using a link
-#Import-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v2-6" -ApiType Http -SpecificationUrl "https://eginternaldevwestus.blob.core.windows.net/packages/EdGraph.EdFi.Ods.App/Core.2.6.0.0.20191010.1/edfi-core-oauth-descriptors-resources-types-enrollment.json?st=2019-10-25T21%3A23%3A51Z&se=2019-10-26T21%3A23%3A51Z&sp=rl&sv=2018-03-28&sr=b&sig=69REitTess2R5qyrn0Qbl%2Fv3rFjPXXRlz3Rtnasy1%2Fk%3D" -SpecificationFormat Swagger -Path "edfi" -ApiVersionSetId 5da9234689b3fbc5f2684379 -ApiVersion v2.6 
-#Import-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v3-2" -ApiType Http -SpecificationUrl "https://eginternaldevwestus.blob.core.windows.net/packages/EdGraph.EdFi.Ods.App/Core.2.6.0.0.20191010.1/edfi-core-oauth-descriptors-resources-types-enrollment.json?st=2019-10-25T21%3A23%3A51Z&se=2019-10-26T21%3A23%3A51Z&sp=rl&sv=2018-03-28&sr=b&sig=69REitTess2R5qyrn0Qbl%2Fv3rFjPXXRlz3Rtnasy1%2Fk%3D" -SpecificationFormat Swagger -Path "edfi" -ApiVersionSetId 5da9234689b3fbc5f2684379 -ApiVersion v3.2 
+#Import-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v2-6" -ApiType Http -SpecificationUrl "https://eginternaldevwestus.blob.core.windows.net/packages/EdGraph.EdFi.Ods.App/Core.2.6.0.0.20191010.1/edfi-core-oauth-descriptors-resources-types-enrollment.json?st=2019-10-25T21%3A23%3A51Z&se=2019-10-26T21%3A23%3A51Z&sp=rl&sv=2018-03-28&sr=b&sig=69REitTess2R5qyrn0Qbl%2Fv3rFjPXXRlz3Rtnasy1%2Fk%3D" -SpecificationFormat Swagger -Path "edfi" -ApiVersionSetId "versionset-edfi-api" -ApiVersion "v2.6"
+#Import-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v3-2" -ApiType Http -SpecificationUrl "https://eginternaldevwestus.blob.core.windows.net/packages/EdGraph.EdFi.Ods.App/Core.2.6.0.0.20191010.1/edfi-core-oauth-descriptors-resources-types-enrollment.json?st=2019-10-25T21%3A23%3A51Z&se=2019-10-26T21%3A23%3A51Z&sp=rl&sv=2018-03-28&sr=b&sig=69REitTess2R5qyrn0Qbl%2Fv3rFjPXXRlz3Rtnasy1%2Fk%3D" -SpecificationFormat Swagger -Path "edfi" -ApiVersionSetId "versionset-edfi-api" -ApiVersion "v3.2" 
 
 #Get APIs
 $apis = Get-AzApiManagementApi -Context $context
 
 #Remove APIs
-$removeEdFiv26 = Remove-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v2-6" -Confirm
-$removeEdFiv32 = Remove-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v3-2" -Confirm
+#$removeEdFiv26 = Remove-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v2-6" -Confirm
+#$removeEdFiv32 = Remove-AzApiManagementApi -Context $context -ApiId "ed-fi-api-v3-2" -Confirm
 
 #Get Version Set
 $versionSets = Get-AzApiManagementApiVersionSet -Context $context
 
 #Remove Version Set
-Remove-AzApiManagementApiVersionSet -Context $context -ApiVersionSetId versionset-edfi-api -Confirm
+#Remove-AzApiManagementApiVersionSet -Context $context -ApiVersionSetId versionset-edfi-api -Confirm
